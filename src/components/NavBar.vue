525aa9247a35c64b635203559017b0e118e06475
Joshua Pena <penajoshua129@gmail.com>
2022-09-16 19:53:19 -0500

    added navigation layout

---
+++ src/components/NavBar.vue
@@ -0,0 +1,42 @@
+<script setup>
+  import { ref } from 'vue'
+  const brand = ref('🏢 Fake Company Directory')
+</script>
+
+<template>
+  <nav>
+    <div class="wrapper">
+      <div class="brand">
+        <span class="brand-title">{{ brand }}</span>
+      </div>
+      <div class="menu">
+        <a href="#" class="menu-item">Departments</a>
+        <a href="#" class="menu-item">Settings</a>
+        <a href="#" class="menu-login">Logout</a>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style scoped lang="postcss">
+  nav {
+    @apply flex h-20 bg-slate-900 text-slate-200;
+    .wrapper {
+      @apply container mx-auto flex w-full items-center justify-between;
+      .brand {
+        &-title {
+          @apply text-2xl font-bold text-green-500;
+        }
+      }
+      .menu {
+        @apply flex gap-2;
+        &-item {
+          @apply rounded-md px-4 py-2 hover:bg-blue-500 hover:text-slate-900;
+        }
+        &-login {
+          @apply rounded-md bg-yellow-500 px-4 py-2 text-yellow-100 hover:bg-yellow-700;
+        }
+      }
+    }
+  }
+</style>
