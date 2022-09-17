a455a695d70c8dae3102baa22fd6592cacb81c9b
Joshua Pena <penajoshua129@gmail.com>
2022-09-16 20:20:53 -0500

    search bar and main changes

---
+++ src/components/MainSearch.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="wrapper">
+    <input type="text" />
+  </div>
+</template>
+
+<style scoped lang="postcss">
+  .wrapper {
+    @apply relative flex w-full items-stretch;
+  }
+</style>
