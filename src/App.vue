01d17c17739d8bfa411439368f041dbef08dc15d
Joshua Pena <penajoshua129@gmail.com>
2023-09-25 12:04:42 -0500

    added cards

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import NavMenu from './components/NavMenu.vue'
 import Main from './components/Main.vue'
+import MainCardsSingle from './components/MainCardsSingle.vue';
 
 </script>
 
 <template>
   <NavMenu />
   <Main />
+  <MainCardsSingle />
   <RouterView />
 </template>
