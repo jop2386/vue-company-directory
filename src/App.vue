e45cbe10d258e16e9a3bf724f32ff4f196a52abd
Joshua Pena <penajoshua129@gmail.com>
2023-09-25 12:31:15 -0500

    deleted mistake

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script setup>
 import NavMenu from './components/NavMenu.vue'
 import Main from './components/Main.vue'
-import MainCardsSingle from './components/MainCardsSingle.vue';
 
 </script>
 
